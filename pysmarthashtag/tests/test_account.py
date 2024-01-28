452243030e6d0e2dbfe19aaf048089a05d56cefe
Bastian Neumann <neumann.bastian@gmail.com>
2024-01-28 16:21:03 +0100

    make use of helper function

--- pysmarthashtag/tests/test_account.py
+++ pysmarthashtag/tests/test_account.py
@@ -1,20 +1,11 @@
-import datetime
-import logging
-from pathlib import Path
-from unittest import mock
-
-import httpx
 import pytest
 import respx
 
-from pysmarthashtag.account import SmartAccount
-
-from . import TEST_PASSWORD, TEST_USERNAME
+from pysmarthashtag.tests.conftest import prepare_account_with_vehicles
 
 
 @pytest.mark.asyncio
 async def test_login(smart_fixture: respx.Router):
     """Test the login flow."""
-    account = SmartAccount(TEST_USERNAME, TEST_PASSWORD)
-    await account.get_vehicles()
+    account = prepare_account_with_vehicles()
     assert account is not None
