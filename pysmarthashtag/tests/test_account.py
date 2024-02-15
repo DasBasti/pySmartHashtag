bcaca94bf190912fef96f5425ee94d394986f813
Bastian Neumann <neumann.bastian@gmail.com>
2024-02-15 23:27:53 +0100

    Test: improve coverage whith a parsing round of vehicle_info

--- pysmarthashtag/tests/test_account.py
+++ pysmarthashtag/tests/test_account.py
@@ -7,5 +7,15 @@ from pysmarthashtag.tests.conftest import prepare_account_with_vehicles
 @pytest.mark.asyncio
 async def test_login(smart_fixture: respx.Router):
     """Test the login flow."""
-    account = prepare_account_with_vehicles()
+    account = await prepare_account_with_vehicles()
     assert account is not None
+
+@pytest.mark.asyncio
+async def test_get_vehicles(smart_fixture: respx.Router):
+    """Test the get_vehicles method."""
+    account = await prepare_account_with_vehicles()
+    assert account is not None
+    assert account.vehicles is not None
+    vehicles = account.vehicles
+    assert len(vehicles) == 1
+    assert vehicles["TestVIN0000000001"].vin == "TestVIN0000000001"
