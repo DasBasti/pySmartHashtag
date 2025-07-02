5237fdd031941954b2e070a17f787ea58a73a88c
Bastian Neumann <DasBasti@users.noreply.github.com>
2025-07-02 21:26:48 +0200

    remove duplicate URL parameter uiLocales
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- pysmarthashtag/const.py
+++ pysmarthashtag/const.py
@@ -1,7 +1,7 @@
 """URLs for different services and error code mapping."""
 
 API_KEY = "3_L94eyQ-wvJhWm7Afp1oBhfTGXZArUfSHHW9p9Pncg513hZELXsxCfMWHrF8f5P5a"
-SERVER_URL = "https://awsapi.future.smart.com/login-app/api/v1/authorize?uiLocales=de-DE&uiLocales=de-DE"
+SERVER_URL = "https://awsapi.future.smart.com/login-app/api/v1/authorize?uiLocales=de-DE"
 AUTH_URL = f"https://auth.smart.com/oidc/op/v1.0/{API_KEY}/authorize/continue"
 LOGIN_URL = "https://auth.smart.com/accounts.login"
 API_BASE_URL = "https://api.ecloudeu.com"
